1837a669bee86be5bb13860d0dfc65fdd84aebc2
vit33235-ux <vit33235@gmail.com>
2025-12-06 21:55:52 +0700

    Delete Bai18.py

--- Bai18.py
+++
@@ -1,9 +0,0 @@
-a = int(input('Nhap a= '))
-b = int(input('Nhap b= '))
-if a < b:
-    uc = a
-else:
-    uc = b
-while (a % uc != 0) or (b % uc != 0):
-    uc = uc - 1
-print('USCLN(',a,',',b,')=',uc)
